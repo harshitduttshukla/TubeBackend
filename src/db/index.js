399b5c0b58a8a161df58c08289d9d5397e52fce4
Harshit Shukla <harshitshukla6388@gmail.com>
2025-03-31 21:04:57 +0530

    add db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import {DB_NAME} from "../constants";
+import {DB_NAME} from "../constants.js";
 
 const connectDB = async () => {
     try {
@@ -8,7 +8,7 @@ const connectDB = async () => {
             ${connetionIntance.connection.host}`);
         
     } catch (error) {
-        console.log("MONGODB connection error",error);
+        console.log("MONGODB connection error111",error);
         process.exit();
     }
 }
